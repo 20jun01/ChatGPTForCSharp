52282167effe834cbef01eb3e5f13f9a33632647
20jun01 <10junjun20@gmail.com>
2023-04-06 06:33:47 +0900

    feat: impl models

---
+++ Assets/Scripts/Chat GPT/Model/ChatGPTCompletionRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chat_GPT.Model
+{
+    //ChatGPT APIにRequestを送るためのJSON用クラス
+    [Serializable]
+    public class ChatGPTCompletionRequestModel
+    {
+        public string model;
+        public List<ChatGPTMessageModel> messages;
+    }
+}
---
+++ Assets/Scripts/Chat GPT/Model/ChatGPTMessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chat_GPT.Model
+{
+    [Serializable]
+    public class ChatGPTMessageModel
+    {
+        public string role;
+        public string content;
+    }
+}
